3612dc140b2cbbfb075b96c64f3bf43dc9ae8c02
root <root@Abishekraghav.localdomain>
2021-02-12 09:39:53 +0530

    message

---
+++ README.md
@@ -0,0 +1,20 @@
+# winbox
+      
+Winbox is an alias script writtes for an automates use for setting up of an your environment for local computer script. If you want to setting up on hacking tools related environment. Please check my other script for more stronger use.
+
+- - -
+
+### Install
+
+
+**Git**
+```
+git clone https://github.com/arhaxor21/winbox.git
+```
+
+### Licensing
+
+**winbox** is licensed under the [MIT license](LICENSE).
+
+
+
---
+++ install.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#Just please absorve the code once and make it install 
+#Mostly i have boxed all the tools
+
+echo "─────────────────────────────────────────────────────────────────────────────────────────────────────────────"
+echo "─██████──────────██████─██████████─██████──────────██████─██████████████───██████████████─████████──████████─"
+echo "─██░░██──────────██░░██─██░░░░░░██─██░░██████████──██░░██─██░░░░░░░░░░██───██░░░░░░░░░░██─██░░░░██──██░░░░██─"
+echo "─██░░██──────────██░░██─████░░████─██░░░░░░░░░░██──██░░██─██░░██████░░██───██░░██████░░██─████░░██──██░░████─"
+echo "─██░░██──────────██░░██───██░░██───██░░██████░░██──██░░██─██░░██──██░░██───██░░██──██░░██───██░░░░██░░░░██───"
+echo "─██░░██──██████──██░░██───██░░██───██░░██──██░░██──██░░██─██░░██████░░████─██░░██──██░░██───████░░░░░░████───"
+echo "─██░░██──██░░██──██░░██───██░░██───██░░██──██░░██──██░░██─██░░░░░░░░░░░░██─██░░██──██░░██─────██░░░░░░██─────"
+echo "─██░░██──██░░██──██░░██───██░░██───██░░██──██░░██──██░░██─██░░████████░░██─██░░██──██░░██───████░░░░░░████───"
+echo "─██░░██████░░██████░░██───██░░██───██░░██──██░░██████░░██─██░░██────██░░██─██░░██──██░░██───██░░░░██░░░░██───"
+echo "─██░░░░░░░░░░░░░░░░░░██─████░░████─██░░██──██░░░░░░░░░░██─██░░████████░░██─██░░██████░░██─████░░██──██░░████─"
+echo "─██░░██████░░██████░░██─██░░░░░░██─██░░██──██████████░░██─██░░░░░░░░░░░░██─██░░░░░░░░░░██─██░░░░██──██░░░░██─"
+echo "─██████──██████──██████─██████████─██████──────────██████─████████████████─██████████████─████████──████████─"
+echo "─────────────────────────────────────────────────────────────────────────────────────────────────────────────"
+
+echo "Dev:abishekraghav Murugeashan"
+
+banner() {
+	echo "+------------------------------------------+"
+	printf "|$(tput bold) %-40s $(tput sgr0)|\n" "$@"
+	echo "+------------------------------------------+"
+}
+
+banner "setting up Resolv config adding"
+
+echo "## OpenDNS ##" >/etc/resolv.conf
+echo "nameserver 208.67.222.222" >>/etc/resolv.conf
+echo "nameserver 208.67.220.220" >>/etc/resolv.conf
+echo "## Google DNS ##" >>/etc/resolv.conf
+echo "nameserver 8.8.8.8" >>/etc/resolv.conf
+echo "nameserver 8.8.4.4" >>/etc/resolv.conf
+
+echo "Resolv config added"
+
+banner "updating linux and packages"
+
+sudo apt update 
+sudo apt upgrade
+sudo apt install python3-pip
+sudo apt install git
+sudo apt install dnsmap
+sudo apt install ipcalc
+sudo apt install gobuster
+sudo apt install ipcalc
+sudo apt install speedtest-cli
+sudo apt install htop
+sudo apt install libcurl4-openssl-dev
+sudo apt install libssl-dev
+sudo apt install jq
+sudo apt install ruby-full
+sudo apt install libxml2 libxml2-dev libxslt1-dev ruby-dev build-essential libgmp-dev zlib1g-dev
+sudo apt install build-essential libssl-dev libffi-dev python-dev
+sudo apt install libldns-dev
+sudo apt install rename
+sudo apt install sqlmap
+sudo apt install nmap
+sudo apt install tree
+banner "Installing tools"
